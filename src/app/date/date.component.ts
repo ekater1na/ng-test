5791accf67e2996fcc153b56ae7627a63eb82f74
ekater1na <ekaterina_l@tut.by>
2019-12-20 13:18:49 +0300

    feat: use ngFor

--- src/app/date/date.component.ts
+++ src/app/date/date.component.ts
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date.component.scss']
 })
 export class DateComponent implements OnInit {
-  message: string 
+  items = ['Angular', 'React', 'Vue', 'Bootstrap', 'Node.js']
+  
   constructor() {
-    setInterval(()=>{
-      this.message = new Date().toLocaleTimeString()
-    }, 1000)
+    
    }
 
   ngOnInit() {
